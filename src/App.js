15b9d08961672fc80d6e4f6bd825d7330840ff07
techsavvyy <glitchfishzero@gmail.com>
2020-04-02 20:29:16 +0500

    feat: Basics in React: reusable components

--- src/App.js
+++ src/App.js
@@ -97,15 +97,35 @@ class Table extends Component {
             <span>{item.author}</span> <span>{item.num_comments}</span>{' '}
             <span>{item.points}</span>{' '}
             <span>
-              <button onClick={() => onDismiss(item.objectID)} type='button'>
+              <Button onClick={() => onDismiss(item.objectID)}>
                 {' '}
                 Dismiss
-              </button>
+              </Button>
             </span>
           </div>
         ))}
       </div>
     );
+  }
+}
+
+class Button extends Component {
+  render() {
+    const {
+      onClick,
+      className,
+      children,
+    } = this.props;
+
+    return (
+      <button
+      onClick={onClick}
+      className={className}
+      type="button"
+      >
+        {children}
+      </button>
+    )
   }
 }
 
