8004c683b4e120e242696dcda48ced94a1b65f4f
techsavvyy <glitchfishzero@gmail.com>
2020-04-02 14:27:16 +0500

    init: start project

---
+++ src/App.js
@@ -0,0 +1,110 @@
+import React, { Component } from 'react';
+import './App.css';
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1
+  }
+];
+
+const isSearched = searchTerm => item =>
+  item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list,
+      searchTerm: '',
+    };
+
+    this.onSearchChange = this.onSearchChange.bind(this);
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value })
+  }
+
+  onDismiss(id) {
+    const isNotId = item => item.objectID !== id;
+    const updatedList = this.state.list.filter(isNotId)
+    this.setState({ list: updatedList });
+  }
+
+  
+  render() {
+    const { searchTerm, list } = this.state;
+    return (
+      <div className='App'>
+      <Search
+      value={searchTerm}
+      onChange={this.onSearchChange}
+      />
+      <Table
+      list={list}
+      pattern={searchTerm}
+      onDismiss={this.onDismiss}
+      />
+      </div>
+    );
+  }
+}
+
+class Search extends Component {
+  render () {
+    const { value, onChange } = this.props;
+    return (
+      <form>
+        <input 
+        type='text'
+        value={value}
+        onChange={onChange}
+        />
+      </form>
+    )
+  }
+}
+
+class Table extends Component {
+  render() {
+    const { list, pattern, onDismiss } = this.props;
+    return (
+      <div>
+        {' '}
+        {list.filter(isSearched(pattern)).map(item => (
+          <div key={item.objectID}>
+            {' '}
+            <span>
+              <a href={item.url}>{item.title}</a>{' '}
+            </span>
+            <span>{item.author}</span> <span>{item.num_comments}</span>{' '}
+            <span>{item.points}</span>{' '}
+            <span>
+              <button onClick={() => onDismiss(item.objectID)} type='button'>
+                {' '}
+                Dismiss
+              </button>
+            </span>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default App;
