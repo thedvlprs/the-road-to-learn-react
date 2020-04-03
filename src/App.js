1f47e7bf36b4567532a92feb6c5e27d47a984722
Sóphie <glitchfishzero@gmail.com>
2020-04-03 23:05:17 +0500

    feat: Getting Real w/API: paginated fetch

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 
 const DEFAULT_QUERY = 'redux';
+const DEFAULT_HPP = '100'
 
 const PATH_BASE = 'https://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
+const PARAM_PAGE = 'page=';
+const PARAM_HPP = 'hitsPerPage=';
 
 class App extends Component {
   constructor(props) {
@@ -24,7 +27,20 @@ class App extends Component {
   }
 
   setSearchTopStories(result) {
-    this.setState({result});
+    const { hits, page } = result;
+
+    const oldHits = page !== 0
+    ? this.state.result.hits
+    : [];
+
+    const updatedHits = [
+      ...oldHits,
+      ...hits
+    ];
+
+    this.setState({
+      result: { hits: updatedHits, page }
+    })
   }
 
   componentDidMount() {
@@ -50,8 +66,8 @@ class App extends Component {
     event.preventDefault();
   }
 
-  fetchSearchTopStories(searchTerm) {
-    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+  fetchSearchTopStories(searchTerm, page = 0) {
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
       .then(response => response.json())
       .then(result => this.setSearchTopStories(result))
       .catch(error => error)
@@ -60,6 +76,7 @@ class App extends Component {
   
   render() {
     const { searchTerm, result } = this.state;
+    const page = (result && result.page) || 0;
 
     return (
       <div className='page'>
@@ -78,6 +95,9 @@ class App extends Component {
           onDismiss={this.onDismiss}
         />
       }
+      <div className='interactions'>
+        <Button onClick={() => this.fetchSearchTopStories(searchTerm, page + 1)}>More</Button>
+      </div>
       </div>
     );
   }
