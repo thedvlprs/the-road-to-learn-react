453244b79a15790a8abf4643f9b93a79bd669f43
techsavvyy <glitchfishzero@gmail.com>
2020-04-06 21:01:49 +0500

    feat: Getting Real w/API: error handling

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ class App extends Component {
       results: null,
       searchKey: '',
       searchTerm: DEFAULT_QUERY,
+      error: null,
     };
 
     this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this);
@@ -88,7 +89,7 @@ class App extends Component {
     fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
       .then(response => response.json())
       .then(result => this.setSearchTopStories(result))
-      .catch(error => error)
+      .catch(error => this.setState({ error }))
   }
 
   needsToSearchTopStories(searchTerm) {
@@ -100,8 +101,10 @@ class App extends Component {
     const { 
       searchTerm, 
       results,
-      searchKey 
+      searchKey,
+      error 
     } = this.state;
+
     const page = (
       results && 
       results[searchKey] &&
@@ -125,11 +128,15 @@ class App extends Component {
             Search 
           </Search>
         </div>
-    
-        <Table
+      {error
+      ? <div className="interactions">
+        <p>Something went wrong.</p>
+      </div>
+      : <Table
           list={list}
           onDismiss={this.onDismiss}
         />
+      }
       <div className='interactions'>
         <Button onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}>More</Button>
       </div>
