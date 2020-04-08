0223603e4a80f5cdb7680b865c8d55e9ca873c33
techsavvyy <glitchfishzero@gmail.com>
2020-04-08 15:01:12 +0500

    feat: Code Organization & Testing: unit tests w/Enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import App, { Search, Button, Table } from './App';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 describe('App', () => {
   it('renders without crashing', () => {
@@ -16,7 +20,7 @@ test('has a valid snapshot', () => {
   );
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
-})
+  });
 })
 
 
@@ -69,5 +73,13 @@ describe('Table', () => {
     const component = renderer.create(<Table { ...props } />);
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  it('shows two items in list', () => {
+    const element = shallow(
+      <Table { ...props } />
+    );
+    
+    expect(element.find('.table-row').length).toBe(2);
   });
 });
